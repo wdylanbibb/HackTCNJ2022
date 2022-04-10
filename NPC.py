e92b90bad8c633c476f8357d13b84a319380bc21
xaridar <xaridara@gmail.com>
2022-04-09 20:15:17 -0400

    populate items

--- NPC.py
+++ NPC.py
@@ -1,12 +1,21 @@
+import names
 from items import Item
+from utils import Point
 
 class NPC:
     def __init__(self, name: str) -> None:
         self.name = name
         self.inventory: list[Item] = []
+
+    def set_position(self, position: Point):
+        self.position = position
+        return self
         
     def addItem(self, item: Item):
         self.inventory.append(item)
 
     def __str__(self) -> str:
         return f'<Person - Name: {self.name}>'
+
+def get_random_NPC():
+    return NPC(names.get_full_name())
